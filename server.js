63f53b29f004fca120748c6cdd87b5a5a6909e1b
Cunfuzed <throwmessagehi@gmail.com>
2026-01-18 19:33:44 -0800

    Change ping endpoint to serve index.html

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (req, res) => {
+app.get("/index.html", (req, res) => {
   res.json({ status: "ok" });
 });
 
