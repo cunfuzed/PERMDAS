d9dad429d0f40ea9841e31a311742c77435b3eff
Cunfuzed <throwmessagehi@gmail.com>
2026-01-18 20:23:24 -0800

    Change route from '/index.html' to '/'

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/index.html", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: "ok" });
 });
 
