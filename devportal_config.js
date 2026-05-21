2f5c62d1739d64586174781a627a14888bb9db73
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-05-20 17:37:07 -0700

    Change devportal_API URL and handle trailing slash
    Update devportal_API URL and ensure no trailing slash.

--- devportal_config.js
+++ devportal_config.js
@@ -1,1 +1,6 @@
-var devportal_API = "https://understood-ports-plate-publication.trycloudflare.com" //urgent: remove slash at the end
+var devportal_API = "https://pda-donor-reggae-labour.trycloudflare.com" //urgent: remove slash at the end!!!!
+
+//of course the programmer forgot
+if (devportal_API.endsWith('/')) {
+  devportal_API = devportal_API.slice(0, -1);
+}
