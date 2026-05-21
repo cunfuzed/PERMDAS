247c5e0868513227866ad7e1d39fa7d2c11564e6
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-05-20 18:06:07 -0700

    Insert favicon link in config.js
    Add favicon link to the HTML document head

--- config.js
+++ config.js
@@ -7,3 +7,11 @@ if (API.endsWith('/')) {
   // slice(0, -1) creates a new string from the start up to the second-to-last character
   API = API.slice(0, -1);
 }
+
+
+//This is where I put the favicon. Since every HTML page runs this, I dont have to edit every page
+var link = document.createElement("link");
+link.rel = "icon";
+link.type = "image/x-icon";
+link.href = "./favicon.ico";
+document.head.appendChild(link);
