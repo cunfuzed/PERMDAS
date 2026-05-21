fce92fd99c10a70dfe080a29162b728213bb77b1
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-05-20 17:36:01 -0700

    Change API endpoint in config.js
    Updated API endpoint to a new URL.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-var API = "https://jacket-comparative-cove-into.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
+var API = "https://arms-added-message-regions.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
 
 
 //of course the programmer forgot
